a96c70c7b7020e1d9d021fc7a443592bc7dac38b
Purrbiscuit <spurr@articulate.com>
2019-11-25 14:20:11 -0500

    add comments to the Game.java file

--- Game.java
+++ Game.java
@@ -1,3 +1,5 @@
+// Gamne class - manages the game object created by the PlayGame driver class
+
 import java.util.ArrayList;
 import java.util.Scanner;
 import javafx.scene.control.Button;
@@ -5,12 +7,14 @@ import javafx.scene.control.Label;
 
 public class Game
 {
+  // create fields for game objects
   private int maxRounds, maxPlayers;
   private int currentPlayer = 1;
   private int currentRound = 1;
   private Player[] players;
   private Dice[] dice;
   
+  // no arg constructor to create a new game with default values
   public Game()
   {
     maxRounds = 3;
@@ -25,6 +29,7 @@ public class Game
       dice[i] = new Dice();
   }
   
+  // constructor to create a new game with custom values passed in
   public Game(int r, int p, int d)
   {
     maxRounds = r;
@@ -39,35 +44,43 @@ public class Game
       dice[i] = new Dice();
   }
   
+  // method to return the currentPlayer for a game
   public int getCurrentPlayer()
   {
     return currentPlayer;
   }
   
+  // method to return the currentRound for a game
   public int getCurrentRound()
   {
     return currentRound;
   }
   
+  // method to return the dice objects used for a game
   public Dice[] getDice()
   {
     return dice;
   }
   
+  // method to return the maxRounds for a game
   public int getMaxRounds()
   {
     return maxRounds;
   }
   
+  // method to return the player objects used for a game
   public Player[] getPlayers()
   {
     return players;
   }
   
+  // nethod which determines if there is another turn left in a game
   public boolean nextTurn()
   {
+    // check if the current player is not the last player
     if (currentPlayer < maxPlayers)
     {
+      // move to the next player if it is
       currentPlayer++;
       return true;
     }
@@ -81,14 +94,20 @@ public class Game
       return true;
     }
     
+    // return false to indicate that there are no more turns left in the game
     return false;
   }
   
-  public static void getResult(Player[] players, Label rollText)
+  // method to determine the result of a game (ie. show winner or ties)
+  private static void getResult(Player[] players, Label rollText)
   {
+    // check what the high score is
     int highScore = highScore(players);
+    // find all players with this score
     ArrayList<Player> highScorePlayers = playersWithHighScore(players, highScore);
     
+    // if only one player has the score then call the showWinner method
+    // else call the showTie method to correctly display tie info
     if (highScorePlayers.size() == 1)
       showWinner(highScorePlayers.get(0), rollText);
     else
@@ -97,7 +116,8 @@ public class Game
     rollText.setStyle("-fx-font-size: 40pt; -fx-text-fill: white");
   }
   
-  public static int highScore(Player[] players)
+  // find the highscore for a game and return the result
+  private static int highScore(Player[] players)
   {
     int highestScore = players[0].getTotalScore();
     
@@ -110,7 +130,8 @@ public class Game
     return highestScore;
   }
   
-  public static ArrayList<Player> playersWithHighScore(Player[] players, int highScore)
+  // return a list of all the players with the highscore for a game
+  private static ArrayList<Player> playersWithHighScore(Player[] players, int highScore)
   {
     ArrayList<Player> highScoreList = new ArrayList<Player>();
 
@@ -123,6 +144,7 @@ public class Game
     return highScoreList;
   }
 
+  // method to end a game and call other methods to determine the result of the game
   public void over(Player[] players, Button roll, Button keep, 
                    Label roundText, Label rollText, Label roundScoreText)
   {    
@@ -136,6 +158,8 @@ public class Game
     Game.getResult(players, rollText);
   }
   
+  // method used to restart a game by setting all the values back to
+  // the original values that are set when a new game is created
   public void restart(Player[] players, Dice[] dice, Label roundScoreText,
                       Label roundText, Label rollText)
   {
@@ -152,6 +176,7 @@ public class Game
       player.setRollNumber(0);
     }
     
+    // call the static dice method to reset all dice
     Dice.resetDice(dice);
     
     roundScoreText.setVisible(true);
@@ -162,18 +187,23 @@ public class Game
     rollText.setStyle("");
   }
   
-  public static void showTie(ArrayList<Player> players, Label rollText)
+  // logic used to correctly show the results of a tie after a game ends
+  private static void showTie(ArrayList<Player> players, Label rollText)
   {
+    // initialize empty string to concatonate values to below
     String playersString = "";
     
     for (int i = 0; i < players.size(); i++)
     {
       Player player = players.get(i);
       
+      // if it's the last player in the list add "and" before to player number
       if ((i + 1) == players.size())
         playersString += (" and " + player.getPlayerNumber());
+      // leave off the comma if it's the second to last player
       else if ((i + 1) == (players.size() - 1))
         playersString += (" " + player.getPlayerNumber());
+      // add a comma to end of string concatonation otherwise after player number
       else
         playersString += (" " + player.getPlayerNumber() + ",");
         
@@ -183,7 +213,8 @@ public class Game
     rollText.setText("Players " + playersString + " Tied!");
   }
 
-  public static void showWinner(Player player, Label rollText)
+  // logic used to correctly show the winner if no ties exists
+  private static void showWinner(Player player, Label rollText)
   {
     rollText.setText("Player " + player.getPlayerNumber() + " Wins!");
     player.highlightWinner();
